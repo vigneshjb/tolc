94b6dd39fd3bbd2809789575a9742997dc94d6a1
Vignesh Jayabalan <vignesh.jayabalan@ticketmaster.com>
2016-02-15 14:10:44 -0700

    init commit - simple crawler

---
+++ crawler.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'nokogiri'
+
+def crawler(url)
+	uri = URI(url)
+	result = "I think the GET failed :(.. I'm confused"
+	result = Net::HTTP.get(uri) unless url==nil
+	html_doc = Nokogiri::HTML(result)
+	all_links = html_doc.css('a').map { |link| link['href']}
+	required_link = []
+	all_links.delete("#")
+	all_links.each_with_index do |link, index| 
+		if (link!=nil && !link.start_with?("#"))
+			required_link.push(link)
+		end
+	end
+end
+
+crawler("https://en.wikipedia.org/wiki/Computer_science")
