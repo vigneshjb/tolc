d8ded239a8b5dbf35222e27bf5a8cf69322fc1c0
Vignesh Jayabalan <vigneshjayabalan@gmail.com>
2016-04-25 00:16:13 -0700

    dashboard UI

--- railsapp/wikispyder/app/helpers/application_helper.rb
+++ railsapp/wikispyder/app/helpers/application_helper.rb
@@ -13,15 +13,42 @@ module ApplicationHelper
     end
   end
 
-  def getUserName(id)
-    return User.find(id).name
+  def getcontent(feed)
+    return_html=""
+    if feed.feed_type == "view"
+      return_html = "<h4><strong>" + getUserName(feed.user_id) + "</strong> has Viewed <code>" + 
+                    getViewedView(feed.feed_content) + "</code></h4> <p class='text-right'> <small>" + 
+                    " This is shown because of your interest in <strong>" + 
+                    ApplicationHelper::INTEREST_HASH[feed.interest] + "</strong></small></p>"
+    elsif feed.feed_type == "request_edit"
+      return_html = "<h4><strong>" + getUserName(feed.user_id) + "</strong> has requested an edit on the wiki<code>" + 
+                    getWikiLink(feed.feed_content) + "</code></h4> <p class='text-right'> <small>" + 
+                    " This is shown because of your interest in <strong>" + 
+                    ApplicationHelper::INTEREST_HASH[feed.interest] + "</strong></small></p>"
+    else
+      return_html = "<h4><strong>" + getUserName(feed.user_id) + "</strong> has suggested content for the wiki <code>" + 
+                    getWikiLink(JSON.parse(feed.feed_content)["page"]) + "</code><br><br><pre>" + 
+                    JSON.parse(feed.feed_content)["content"] + "</pre></h4> <p class='text-right'> <small>" + 
+                    " This is shown because of your interest in <strong>" + 
+                    ApplicationHelper::INTEREST_HASH[feed.interest] + "</strong></small></p>"
+
+    end
+    return return_html
   end
 
-  def getWikiURLS(viewChain)
+  def getUserName(id)
+    return User.find(id).name.titleize
+  end
+
+  def getViewedView(viewChain)
     viewItems = viewChain.split(',')
     urlString = ""
-    viewItems.each { |item| urlString += "<a href=\"https://en.wikipedia.org/wiki/"+item.gsub(' ','_')+"\">"+item+"</a>" +" > " }
+    viewItems.each { |item| urlString += "<a href=\"https://en.wikipedia.org/wiki/"+item.gsub(' ','_')+"\">"+item.titleize+"</a>" +" > " }
     urlString[0..-3]
+  end
+
+  def getWikiLink(pageName)
+    "<a href=\"https://en.wikipedia.org/wiki/"+pageName.gsub(' ','_')+"\">"+pageName.titleize+"</a>"
   end
 
 end
